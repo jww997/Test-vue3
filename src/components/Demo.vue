c0f11975639500d60e2dbcd7b5b3dd1610d2c497
chenjw <1551005987@qq.com>
2021-07-15 09:44:09 +0800

    feat:watch监听ref定义的数据

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,48 +1,53 @@
 <template>
-  <h1>一个人的信息：</h1>
-  姓：<input type="text" v-model="person.firstName" />
-  <br />
-  名：<input type="text" v-model="person.lastName" />
-  <br />
-  <span>全名：{{ person.fullName }}</span>
-  <br />
-  全名：<input type="text" v-model="person.fullName" />
+  <h2>当前求和为：{{ sum }}</h2>
+  <button @click="sum++">点我+1</button>
+  <hr />
+  <h2>当前的信息为：{{ msg }}</h2>
+  <button @click="msg += '!'">修改信息</button>
 </template>
 
 <script>
-import { reactive, computed } from "vue";
+import { ref, watch } from "vue";
 export default {
   name: "Demo",
-  // computed: {
-  //   fullName() {
-  //     return this.person.firstName + "-" + this.person.lastName;
+  // watch: {
+  //   sum(newValue, oldValue) {
+  //     console.log("sum的值变化了", newValue, oldValue);
+  //   },
+  //   sum: {
+  //     immediate: true,
+  //     deep: true,
+  //     handler(newValue, oldValue) {
+  //       console.log("sum的值变化了", newValue, oldValue);
+  //     },
   //   },
   // },
   setup() {
     // 数据
-    let person = reactive({
-      firstName: "张",
-      lastName: "三",
-    });
-    // 计算属性--简写（没有考虑计算属性被修改的情况）
-    // person.fullName = computed(() => {
-    //   return person.firstName + "-" + person.lastName;
-    // });
-    // 计算属性--完整写法（考虑读和写）
-    person.fullName = computed({
-      get() {
-        return person.firstName + "-" + person.lastName;
+    let sum = ref(0);
+    let msg = ref("你好啊");
+
+    // 情况一：监视ref所定义的一个响应式数据
+    // watch(
+    //   sum,
+    //   (newValue, oldValue) => {
+    //     console.log("sum变化了", newValue, oldValue);
+    //   },
+    //   { immediate: true }
+    // );
+    // 情况二：监视ref所定义的多个响应式数据
+    watch(
+      [sum, msg],
+      (newValue, oldValue) => {
+        console.log("sum或msg变化了", newValue, oldValue);
       },
-      set(value) {
-        const nameArr = value.split("-");
-        person.firstName = nameArr[0];
-        person.lastName = nameArr[1];
-      },
-    });
+      { immediate: true }
+    );
 
     // 返回一个对象（常用）
     return {
-      person,
+      sum,
+      msg,
     };
   },
 };
