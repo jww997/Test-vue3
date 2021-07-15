601c28598127b005352ad3764b4aa59193bcb075
chenjw <1551005987@qq.com>
2021-07-15 13:55:20 +0800

    feat:自定义hook

---
+++ 12_src_自定义hook/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>我是Test组件</h2>
+  <h2>当前点击时鼠标的坐标为：x:{{ point.x }} y:{{ point.y }}</h2>
+</template>
+
+<script>
+// import { ref } from "@vue/reactivity";
+import usePoint from "../hooks/usePoint";
+export default {
+  name: "Test",
+  setup() {
+    // let name = ref("tom");
+    const point = usePoint();
+    // return { name, point };
+    return { point };
+  },
+};
+</script>
