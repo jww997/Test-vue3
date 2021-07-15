ce9d154e1d7506f3ab930b91b4a476cc9985e349
chenjw <1551005987@qq.com>
2021-07-15 09:13:53 +0800

    feat:setup的两个注意点

---
+++ src/components/Demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>一个人的信息：</h1>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <button @click="test">测试触发一下Demo组件的Hello事件</button>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "Demo",
+  props: ["msg", "school"],
+  emits: ["hello"],
+  setup(props, context) {
+    console.log("---setup---", props);
+    console.log("---setup---", context);
+    console.log("---setup---", context.attrs); // 相当于与vue2中$attrs
+    console.log("---setup---", context.emit); // 触发自定义事件的
+    console.log("---setup---", context.slots);
+
+    // 数据
+    let person = reactive({
+      name: "张三",
+      age: 18,
+    });
+
+    // 方法
+    function test() {
+      context.emit("hello", 666);
+    }
+
+    return {
+      person,
+      test,
+    };
+  },
+};
+</script>
