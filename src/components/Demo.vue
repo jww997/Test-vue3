4f234c1bbe7fb8bc3b8f9076a660a31ee978c828
chenjw <1551005987@qq.com>
2021-07-15 10:13:13 +0800

    feat:watch监听reactive定义的数据

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -4,10 +4,17 @@
   <hr />
   <h2>当前的信息为：{{ msg }}</h2>
   <button @click="msg += '!'">修改信息</button>
+  <hr />
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄：{{ person.age }}</h2>
+  <h2>薪资：{{ person.job.j1.salary }}k</h2>
+  <button @click="person.name += '~'">修改姓名</button>
+  <button @click="person.age++">增长年龄</button>
+  <button @click="person.job.j1.salary++">涨薪</button>
 </template>
 
 <script>
-import { ref, watch } from "vue";
+import { ref, reactive, watch } from "vue";
 export default {
   name: "Demo",
   // watch: {
@@ -26,6 +33,11 @@ export default {
     // 数据
     let sum = ref(0);
     let msg = ref("你好啊");
+    let person = reactive({
+      name: "张三",
+      age: 18,
+      job: { j1: { salary: 20 } },
+    });
 
     // 情况一：监视ref所定义的一个响应式数据
     // watch(
@@ -36,18 +48,54 @@ export default {
     //   { immediate: true }
     // );
     // 情况二：监视ref所定义的多个响应式数据
+    // watch(
+    //   [sum, msg],
+    //   (newValue, oldValue) => {
+    //     console.log("sum或msg变化了", newValue, oldValue);
+    //   },
+    //   { immediate: true }
+    // );
+
+    /**
+     * 情况三：监视reactive所定义的一个响应式数据，注意此处无法正确的获取oldValue
+     *    1. 注意：此处无法正确获取oleValue
+     *    2. 注意：强制开启了深度监视（deep配置无效）
+     */
     watch(
-      [sum, msg],
+      person,
       (newValue, oldValue) => {
-        console.log("sum或msg变化了", newValue, oldValue);
+        console.log("person变化了", newValue, oldValue);
       },
-      { immediate: true }
+      { deep: false } // 此处的deep配置无效
+    );
+
+    // 情况四：监视reactive所定义的一个响应式数据中的某个属性
+    // watch(
+    //   () => person.age,
+    //   (newValue, oldValue) => {
+    //     console.log("person的age变化了", newValue, oldValue);
+    //   }
+    // );
+
+    // 情况五：监听reactive所定义的一个响应式数据中的某个属性
+    // watch([() => person.name, () => person.age], (newValue, oldValue) => {
+    //   console.log("person的name或age变化了", newValue, oldValue);
+    // });
+
+    // 特殊情况
+    watch(
+      () => person.job,
+      (newValue, oldValue) => {
+        console.log("person的job变化了", newValue, oldValue);
+      },
+      { deep: true } // 此处由于监视的是reactive是定义对象中的某个属性，所以deep配置有效
     );
 
     // 返回一个对象（常用）
     return {
       sum,
       msg,
+      person,
     };
   },
 };
