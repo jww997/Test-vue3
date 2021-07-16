61591d9b37fdef8e961d2321695b4dc685fd9f54
chenjw <1551005987@qq.com>
2021-07-16 10:14:34 +0800

    feat:Provide与Inject

---
+++ 18_src_provide与inject/components/Son.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="son">我是Son组件（孙）. {{ car.name }}-{{ car.price }}</div>
+</template>
+
+<script>
+import { inject, toRefs } from "vue";
+export default {
+  name: "Son",
+  setup() {
+    let car = inject("car");
+    return {
+      car,
+      // ...toRefs(car),
+    };
+  },
+};
+</script>
+
+<style>
+.son {
+  background-color: orange;
+  padding: 10px;
+}
+</style>
