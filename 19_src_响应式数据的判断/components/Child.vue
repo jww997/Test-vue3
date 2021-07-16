85361a22b1a2d1618e2b971d45278bf78d8ad235
chenjw <1551005987@qq.com>
2021-07-16 10:21:39 +0800

    feat:响应式数据的判断

---
+++ 19_src_响应式数据的判断/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件（子）</h3>
+    <Son />
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+import Son from "./Son";
+export default {
+  name: "Child",
+  components: {
+    Son,
+  },
+  setup() {
+    let x = inject("car");
+    console.log(x);
+  },
+};
+</script>
+
+<style>
+.child {
+  background-color: skyblue;
+  padding: 10px;
+}
+</style>
